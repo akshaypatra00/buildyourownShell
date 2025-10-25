75cab4af0fa27e64e8e6ac151cd915552cb469cb
akshay patra <Akshay>
2025-10-25 11:37:49 +0530

    add REPL loop

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    
-    # Wait for user input
-    command = input()
-    
-    # Print error message
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        
+        # Wait for user input
+        command = input()
+        
+        # Print error message
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
